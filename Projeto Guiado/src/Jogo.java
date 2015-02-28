80b03ef7f0c363559006798633c433e85350a97f
gtugablue <gtugablue@gmail.com>
2015-02-28 19:04:39 +0000

    Separação entre classes Jogo e Labirinto

--- Projeto Guiado/src/Jogo.java
+++ Projeto Guiado/src/Jogo.java
@@ -1,66 +1,166 @@
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+
 public class Jogo {
-	private static int MAP_SIDE;
-	private static final int MIN_MAP_SIDE = 8;
-	private static int MIN_ELEM_DIST;
-	private static final int ELEM_DIST_FACTOR = 2;
+
+	////////////////////////////////
+	///////   Atributes   //////////
+	////////////////////////////////
 	
-	private static int heroi_x, heroi_y, dragao_x, dragao_y;
-	private static char parede_char = 'X';
-	private static char dragao_char = 'D';
-	private static char dragao_espada_char = 'F';
-	private static char heroi_char = 'H';
-	private static char heroi_armado_char = 'A';
-	private static char saida_char = 'S';
-	private static char espada_char = 'E';
-	private static int saida_x, saida_y, espada_x, espada_y;
-	private static boolean heroi_armado, dragao;
 
-	private static Scanner s;
+	private static final int ELEM_DIST_FACTOR = 2;			// Fator usado para determinar a dist. mínima entre elementos
 
-	private static char[][] matrix;
+	private static final char dragao_char = 'D';			// Símbolo representativo do dragão
+	private static final char espada_char = 'E';			// Símbolo da espada
+	private static final char dragao_espada_char = 'F';		// Símbolo a representar quando dragão e espada estão coincidentes
+	private static final char heroi_char = 'H';				// Símbolo do herói sem espada
+	private static final char heroi_armado_char = 'A';		// Símbolo do herói com espada
+
+	private static int heroi_x, heroi_y;			// Coordenadas do herói
+	private static int dragao_x, dragao_y;			// Coordenadas do dragão
+	private static int espada_x, espada_y;			// Coordenadas da espada
+	private static boolean heroi_armado;			// True se o heroi já está armado
+	private static boolean dragao;					// True se o dragão está vivo
+
+	private static Scanner s;						// Used to read from the console throughout the program
+
+	private static Labirinto map;					// Represents the game map
+
+	
+	
+
+	////////////////////////////////
+	///////   Functions   //////////
+	////////////////////////////////
+	
 	
 	public static void main(String[] args) {
-		heroi_x = 1;
-		heroi_y = 1;
-		saida_x = 9;
-		saida_y = 5;
-		espada_x = 1;
-		espada_y = 8;
-		dragao_x = 1;
-		dragao_y = 3;
+
 		heroi_armado = false;
 		dragao = true;
 
 		s = new Scanner(System.in);
-		
+
+
 		System.out.print("For this game, you can choose the dimmensions of the game map. The minimum size is 8,\nsince smaller sizes would make the game impossible to finish.\n");
-		System.out.print("Please indicate the map size (minimum " + MIN_MAP_SIDE + ") : ");
-		MAP_SIDE = s.nextInt();
-		
-		while(MAP_SIDE < MIN_MAP_SIDE)
+		System.out.print("Please indicate the map size (minimum " + Labirinto.MIN_REC_SIDE + ") : ");
+		int map_side = s.nextInt();
+
+		while(map_side < Labirinto.MIN_REC_SIDE)
 		{
-			System.out.print("\nGiven value is smaller than " + MIN_MAP_SIDE + ".\nPlease insert new value : ");
-			MAP_SIDE = s.nextInt();
+			System.out.print("\nGiven value is greater than " + Labirinto.MIN_REC_SIDE + ".\nPlease insert new value : ");
+			map_side = s.nextInt();
 		}
 
-		MIN_ELEM_DIST = (int) (MAP_SIDE/ELEM_DIST_FACTOR);
-		MIN_ELEM_DIST = MIN_ELEM_DIST*MIN_ELEM_DIST;
-		generateMap(MAP_SIDE);
-		
+		int minElemDist = (int) (map_side/ELEM_DIST_FACTOR);
+		minElemDist = minElemDist*minElemDist;
+
+		map = new Labirinto(map_side);
+		generateMapElements(minElemDist);
+
 		do{
 			drawMap();
 		}while(turn());
-		//drawMap();
-
+		 
 		s.close();
 	}
 
 	private static void drawMap()
 	{
-		for(int i = 0; i < MAP_SIDE; i++)
+		boolean saida = !dragao;
+		
+		int[] coords = new int[6];
+		Arrays.fill(coords, -1);
+		char[] chars = new char[3];
+		Arrays.fill(chars, ' ');
+		
+		
+		coords[0] = heroi_x;
+		coords[1] = heroi_y;
+		if(heroi_armado)
+			chars[0] = heroi_armado_char;
+		else
+			chars[0] = heroi_char;
+		
+		int i = 1;
+		
+
+		if(espada_x == dragao_x && espada_y == dragao_y)
+		{
+			coords[2*i] = espada_x;
+			coords[2*i+1] = espada_y;
+			
+			if(!heroi_armado)
+				chars[i] = dragao_espada_char;
+			else
+				chars[i] = dragao_char;
+			i++;
+		}
+		else
+		{
+			if(!heroi_armado)
+			{
+				coords[2*i] = espada_x;
+				coords[2*i + 1] = espada_y;
+				chars[i] = espada_char;
+				i++;
+			}
+			
+			if(dragao)
+			{
+				coords[2*i] = dragao_x;
+				coords[2*i + 1] = dragao_y;
+				chars[i] = dragao_char;
+				i++;
+			}
+		}
+		
+		map.drawMatrix(coords, chars, saida);
+		
+		
+		
+		
+		
+		
+		
+
+		/*if(espada_x == dragao_x && espada_y == dragao_y)
+		{
+			coords = new int[4];
+			chars = new char[2];
+			
+			coords[0] = heroi_x;
+			coords[1] = heroi_y;
+			if(heroi_armado)
+				chars[0] = heroi_armado_char;
+			else
+				chars[0] = heroi_char;
+			
+			coords[2] = espada_x;
+			coords[3] = espada_y;
+			chars[1] = dragao_espada_char;
+		}
+		else
+		{
+			coords = new int[6];
+			chars = new char[3];
+			
+			coords[0] = heroi_x;
+			coords[1] = heroi_y;
+			if(heroi_armado)
+				chars[0] = heroi_armado_char;
+			else
+				chars[0] = heroi_char;
+			
+			coords[2] = espada_x;
+			coords[3] = espada_y;
+			chars[1] = dragao_espada_char;
+		}
+		
+		
+		
+		/*for(int i = 0; i < MAP_SIDE; i++)
 		{
 			for(int j = 0; j < MAP_SIDE; j++)
 			{
@@ -88,7 +188,7 @@ public class Jogo {
 				System.out.print(' ');
 			}
 			System.out.print('\n');
-		}
+		}*/
 	}
 
 	private static boolean turn()
@@ -103,7 +203,8 @@ public class Jogo {
 			moverHeroi(heroi_x, heroi_y + 1);
 		else if(key.toUpperCase().equals("D"))
 			moverHeroi(heroi_x+1, heroi_y);
-		if(heroi_x == saida_x && heroi_y == saida_y) 
+		
+		if(map.isSaida(heroi_x, heroi_y)) 
 		{
 			System.out.print("\n\n Congratulations! You escaped the maze!\n\n");
 			return false;
@@ -125,13 +226,13 @@ public class Jogo {
 
 		if(combateDragao())
 		{
-			
 			return true;
 		}
 
 		System.out.print("\n\n Bad luck! The dragon killed you... Take revenge next time!\n\n");
 		return false;
 	}
+
 	private static void turnDragao()
 	{
 		Random x = new Random();
@@ -160,7 +261,7 @@ public class Jogo {
 	}
 
 	private static boolean moverDragao(int x, int y) {
-		if (!isParede(x, y))
+		if (!map.isParede(x, y))
 		{
 			dragao_x = x;
 			dragao_y = y;
@@ -170,7 +271,7 @@ public class Jogo {
 	}
 
 	private static boolean moverHeroi(int x, int y) {
-		if(!isParede(x, y) || (x == saida_x && y == saida_y && !dragao))
+		if(!map.isParede(x, y) || (map.isSaida(x,  y) && !dragao))
 		{
 			heroi_x=x;
 			heroi_y=y;
@@ -178,17 +279,10 @@ public class Jogo {
 		}
 		return false;
 	}
-	private static boolean isParede(int x, int y) {
-		return matrixCoord(x, y) == parede_char;
-	}
-	private static char matrixCoord(int x, int y)
-	{
-		return matrix[y][x];
-	}
 
 	private static boolean combateDragao()
 	{
-		if(verificaAdjacentes(heroi_x, heroi_y, dragao_x, dragao_y) && dragao)
+		if(Labirinto.areAdjacent(heroi_x, heroi_y, dragao_x, dragao_y) && dragao)
 		{
 			if(heroi_armado)
 				dragao = false;
@@ -198,228 +292,57 @@ public class Jogo {
 		return true;
 	}
 
-	private static boolean verificaAdjacentes(int x1, int y1, int x2, int y2)
-	{
-		if(x1 == x2)
-		{
-			if(Math.abs(y2-y1)==1)
-				return true;
-		}
-		else if(y1 == y2)
-		{
-			if(Math.abs(x2-x1)==1)
-				return true;
-		}
-
-		return false;
-	}
-	private static void generateMap(int side) {
-
-		matrix = new char[side][side];
-		fillMap(side);
-		generateMapExit(side);
-		int start_x, start_y;
-		if (saida_x == 0)
-		{
-			start_x = 1;
-			start_y = saida_y;
-		}
-		else if (saida_y == 0)
-		{
-			start_x = saida_x;
-			start_y = 1;
-		}
-		else if (saida_x == side - 1)
-		{
-			start_x = side - ((side % 2 == 0) ? 3 : 2);
-			start_y = saida_y;
-		}
-		else
-		{
-			start_x = saida_x;
-			start_y = side - ((side % 2 == 0) ? 3 : 2);
-		}
-		matrix[start_y][start_x] = ' ';
-		generateMapWalls(side, start_x, start_y);
-
-		generateMapFixEven(side);
-		
-		generateMapElements();
-
-	}
-	private static void generateMapFixEven(int side) {
-		// Fix for even side
-		if (side % 2 == 0)
-		{
-			Random r = new Random();
-			int n;
-
-			// Duplicate column
-			n = r.nextInt(side - 4) + 2;
-			for (int x = side - 2; x > n; x--) {
-				for (int y = 0; y < side; y++) {
-					matrix[y][x] = matrix[y][x - 1];
-				}
-			}
-			for (int y = 0; y < side; y++) {
-				if (matrix[y][n - 1] != matrix[y][n + 1])
-					matrix[y][n] = parede_char;
-			}
-
-			// Correct exit position
-			if (saida_x < side - 1 && n <= saida_x)
-				saida_x++;
-
-			// Duplicate line
-			n = r.nextInt(side - 4) + 2;
-			for (int y = side - 2; y > n; y--) {
-				for (int x = 0; x < side; x++) {
-					matrix[y][x] = matrix[y - 1][x];
-				}
-			}
-			for (int x = 0; x < side; x++) {
-				if (matrix[n - 1][x] != matrix[n + 1][x])
-					matrix[n][x] = parede_char;
-			}
-			// Correct exit position
-			if (saida_y < side - 1 && n <= saida_y)
-				saida_y++;
-		}
-	}
-
-	private static void generateMapExit(int side) {
-		Random r = new Random();
-		int n = 2 * r.nextInt(side / 2 - 2) + 1;
-		switch(r.nextInt(4))
-		{
-		case 0:
-			saida_y = 0;
-			saida_x = n;
-			break;
-		case 1:
-			saida_y = side - 1;
-			saida_x = n;
-			break;
-		case 2:
-			saida_x = 0;
-			saida_y = n;
-			break;
-		case 3:
-			saida_x = side - 1;
-			saida_y = n;
-			break;
-		}
-	}
-
-	private static void fillMap(int side) {
-		for (int y = 0; y < side; y++) {
-			for (int x = 0; x < side; x++) {
-				matrix[x][y] = parede_char;
-			}
-		}
-	}
-
-	private static void generateMapWalls(int side, int x, int y) {
-		int[] directions = {0, 1, 2, 3};
-		directions = randomizeArray(directions);
-		for (int i = 0; i < directions.length; i++) {
-			switch (directions[i]) {
-			case 0: // cima
-				if (removeMapWall(side, x, y, x, y - 2))
-					generateMapWalls(side, x, y - 2);
-				break;
-			case 1: // baixo
-				if (removeMapWall(side, x, y, x, y + 2))
-					generateMapWalls(side, x, y + 2);
-				break;
-			case 2: // esquerda
-				if (removeMapWall(side, x, y, x - 2, y))
-					generateMapWalls(side, x - 2, y);
-				break;
-			case 3: // direita
-				if (removeMapWall(side, x, y, x + 2, y))
-					generateMapWalls(side, x + 2, y);
-				break;
-			}
-		}
-	}
-
-	private static boolean removeMapWall(int side, int x, int y, int new_x, int new_y) {
-		if (new_x > 0 && new_x < side - 1 && new_y > 0 && new_y < side - 1 && matrix[new_y][new_x] == parede_char)
-		{
-			matrix[new_y][new_x] = ' ';
-			matrix[(new_y + y) / 2][(new_x + x) / 2] = ' ';
-			return true;
-		}
-		return false;
-	}
-
-	private static int[] randomizeArray(int[] array) {
-		Random r = new Random();
-		for (int i = 0; i < array.length; i++) {
-			int n = r.nextInt(array.length);
-			int val = array[i];
-			array[i] = array[n];
-			array[n] = val;
-		}
-		return array;
-	}
-
-	private static void generateMapElements()
+	private static void generateMapElements(int min_dist)
 	{
 		generatePosHeroi();
-		generatePosEspada();
-		generatePosDragao();
-		
+		generatePosEspada(min_dist);
+		generatePosDragao(min_dist);
+
 	}
 
 	private static void generatePosHeroi() {
 		Random rand = new Random();
-		
+
 		int randX, randY;
-		
+
 		do
 		{
-			randX = rand.nextInt(MAP_SIDE-2) + 1;
-			randY = rand.nextInt(MAP_SIDE-2) + 1;			
-		} while(isParede(randX, randY) || randX >= MAP_SIDE || randY >= MAP_SIDE);
-		
+			randX = rand.nextInt(map.matrixSide-2) + 1;
+			randY = rand.nextInt(map.matrixSide-2) + 1;			
+		} while(map.isParede(randX, randY) || randX >= map.matrixSide || randY >= map.matrixSide);
+
 		heroi_x = randX;
 		heroi_y = randY;
 	}
-	
-	private static void generatePosEspada() {
+
+	private static void generatePosEspada(int min_dist) {
 		Random rand = new Random();
-		
+
 		int randX, randY;
-		
+
 		do
 		{
-			randX = rand.nextInt(MAP_SIDE-2) + 1;
-			randY= rand.nextInt(MAP_SIDE-2) + 1;			
-		} while(isParede(randX, randY) || coordDistSquare(randX, randY, heroi_x, heroi_y) < MIN_ELEM_DIST || randX >= MAP_SIDE || randY >= MAP_SIDE );
-		
+			randX = rand.nextInt(map.matrixSide-2) + 1;
+			randY= rand.nextInt(map.matrixSide-2) + 1;			
+		} while(map.isParede(randX, randY) || Labirinto.coordDistSquare(randX, randY, heroi_x, heroi_y) < min_dist);
+
 		espada_x = randX;
 		espada_y = randY;
 	}
-	
-	private static void generatePosDragao() {
+
+	private static void generatePosDragao(int min_dist) {
 		Random rand = new Random();
 
 		int randX, randY;
 
 		do
 		{
-			randX = rand.nextInt(MAP_SIDE-2) + 1;
-			randY= rand.nextInt(MAP_SIDE-2) + 1;			
-		} while(isParede(randX, randY) || coordDistSquare(randX, randY, heroi_x, heroi_y) < MIN_ELEM_DIST || randX >= MAP_SIDE || randY >= MAP_SIDE );
+			randX = rand.nextInt(map.matrixSide-2) + 1;
+			randY= rand.nextInt(map.matrixSide-2) + 1;			
+		} while(map.isParede(randX, randY) || Labirinto.coordDistSquare(randX, randY, heroi_x, heroi_y) < min_dist);
 
 		dragao_x = randX;
 		dragao_y = randY;
 	}
-	
-	private static int coordDistSquare(int x1, int y1, int x2, int y2)
-	{
-		return (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
-	}
+
 }
---
+++ Projeto Guiado/src/Labirinto.java
@@ -0,0 +1,244 @@
+import java.util.Random;
+
+
+public class Labirinto {
+	
+	public static final int MIN_REC_SIDE = 8;
+	private static final char paredeChar = 'X';
+	private static final char saidaChar = 'S';
+	
+	public int matrixSide;
+	public char[][] matrix;
+	public int saidaX, saidaY;
+	
+	public Labirinto(int matrix_side)
+	{
+		matrixSide = matrix_side;
+		generateMatrix();
+	}
+	
+	public void drawMatrix(int[] coords, char[] chars, boolean saida)
+	{
+		boolean printed = false;
+		
+		for(int i = 0; i < matrixSide; i++)
+		{
+			for(int j = 0; j < matrixSide; j++)
+			{
+				for(int coord = 0; coord < chars.length; coord++)
+				{
+					if(j==coords[2*coord] && i == coords[2*coord + 1])
+					{
+						printed=true;
+						System.out.print(chars[coord]);
+						break;
+					}
+				}
+				
+				if(!printed)
+				{
+					if (j == saidaX && i == saidaY && saida)
+						System.out.print(saidaChar);
+					else
+						System.out.print(matrix[i][j]);						
+				}
+				
+				printed = false;
+				System.out.print(' ');
+			}
+			System.out.print('\n');
+		}
+	}
+	
+	public char matrixCoords(int x, int y)
+	{
+		return matrix[y][x];
+	}
+
+	public boolean isParede(int x, int y)
+	{
+		return matrixCoords(x, y) == paredeChar;
+	}
+	
+	public boolean isSaida(int x, int y)
+	{
+		return matrixCoords(x, y) == saidaChar;
+	}
+	
+	public static int coordDistSquare(int x1, int y1, int x2, int y2)
+	{
+		return (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+	}
+	
+	public static boolean areAdjacent(int x1, int y1, int x2, int y2)
+	{
+		if(x1 == x2)
+		{
+			if(Math.abs(y2-y1)==1)
+				return true;
+		}
+		else if(y1 == y2)
+		{
+			if(Math.abs(x2-x1)==1)
+				return true;
+		}
+
+		return false;
+	}
+	
+ 	private void generateMatrix()
+	{
+		matrix = new char[matrixSide][matrixSide];
+		fillMatrix(paredeChar);
+		
+		generateMapExit();
+		
+		int start_x, start_y;
+		
+		if (saidaX == 0)
+		{
+			start_x = 1;
+			start_y = saidaY;
+		}
+		else if (saidaY == 0)
+		{
+			start_x = saidaX;
+			start_y = 1;
+		}
+		else if (saidaX == matrixSide - 1)
+		{
+			start_x = matrixSide - 3;
+			start_y = saidaY;
+		}
+		else
+		{
+			start_x = saidaX;
+			start_y = matrixSide - 3;
+		}
+		matrix[start_y][start_x] = ' ';
+		
+		generateMapWalls(start_x, start_y);
+
+		generateMapFixEven();
+	}
+
+	private void fillMatrix(char fill) {
+		for (int y = 0; y < matrixSide; y++) {
+			for (int x = 0; x < matrixSide; x++) {
+				matrix[x][y] = fill;
+			}
+		}
+	}
+
+	private void generateMapExit() {
+		Random r = new Random();
+		int n = 2 * r.nextInt(matrixSide / 2 - 2) + 1;
+		switch(r.nextInt(4))
+		{
+		case 0:
+			saidaY = 0;
+			saidaX = n;
+			break;
+		case 1:
+			saidaY = matrixSide - 1;
+			saidaX = n;
+			break;
+		case 2:
+			saidaX = 0;
+			saidaY = n;
+			break;
+		case 3:
+			saidaX = matrixSide - 1;
+			saidaY = n;
+			break;
+		}
+	}
+	
+	private void generateMapWalls(int x, int y) {
+		int[] directions = {0, 1, 2, 3};
+		directions = randomizeArray(directions);
+		for (int i = 0; i < directions.length; i++) {
+			switch (directions[i]) {
+			case 0: // cima
+				if (removeMapWall(x, y, x, y - 2))
+					generateMapWalls(x, y - 2);
+				break;
+			case 1: // baixo
+				if (removeMapWall(x, y, x, y + 2))
+					generateMapWalls(x, y + 2);
+				break;
+			case 2: // esquerda
+				if (removeMapWall(x, y, x - 2, y))
+					generateMapWalls(x - 2, y);
+				break;
+			case 3: // direita
+				if (removeMapWall(x, y, x + 2, y))
+					generateMapWalls(x + 2, y);
+				break;
+			}
+		}
+	}
+	
+	private boolean removeMapWall(int x, int y, int new_x, int new_y) {
+		if (new_x > 0 && new_x < matrixSide - 1 && new_y > 0 && new_y < matrixSide - 1 && matrix[new_y][new_x] == paredeChar)
+		{
+			matrix[new_y][new_x] = ' ';
+			matrix[(new_y + y) / 2][(new_x + x) / 2] = ' ';
+			return true;
+		}
+		return false;
+	}
+	
+	private static int[] randomizeArray(int[] array) {
+		Random r = new Random();
+		for (int i = 0; i < array.length; i++) {
+			int n = r.nextInt(array.length);
+			int val = array[i];
+			array[i] = array[n];
+			array[n] = val;
+		}
+		return array;
+	}
+	
+	private void generateMapFixEven() {
+		// Fix for even side
+		if (matrixSide % 2 == 0)
+		{
+			Random r = new Random();
+			int n;
+
+			// Duplicate column
+			n = r.nextInt(matrixSide - 4) + 2;
+			for (int x = matrixSide - 2; x > n; x--) {
+				for (int y = 0; y < matrixSide; y++) {
+					matrix[y][x] = matrix[y][x - 1];
+				}
+			}
+			for (int y = 0; y < matrixSide; y++) {
+				if (matrix[y][n - 1] != matrix[y][n + 1])
+					matrix[y][n] = paredeChar;
+			}
+
+			// Correct exit position
+			if (saidaX < matrixSide - 1 && n <= saidaX)
+				saidaX++;
+
+			// Duplicate line
+			n = r.nextInt(matrixSide - 4) + 2;
+			for (int y = matrixSide - 2; y > n; y--) {
+				for (int x = 0; x < matrixSide; x++) {
+					matrix[y][x] = matrix[y - 1][x];
+				}
+			}
+			for (int x = 0; x < matrixSide; x++) {
+				if (matrix[n - 1][x] != matrix[n + 1][x])
+					matrix[n][x] = paredeChar;
+			}
+			// Correct exit position
+			if (saidaY < matrixSide - 1 && n <= saidaY)
+				saidaY++;
+		}
+	}
+	
+	
+}
