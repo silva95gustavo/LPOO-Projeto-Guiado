56ee9c19d2a480116dbafede974c2bd420e7f46f
gtugablue <gtugablue@gmail.com>
2015-02-28 19:06:05 +0000

    Map generation bug fix

--- Projeto Guiado/src/Labirinto.java
+++ Projeto Guiado/src/Labirinto.java
@@ -107,13 +107,13 @@ public class Labirinto {
 		}
 		else if (saidaX == matrixSide - 1)
 		{
-			start_x = matrixSide - 3;
+			start_x = matrixSide - ((side % 2 == 0) ? 3 : 2);
 			start_y = saidaY;
 		}
 		else
 		{
 			start_x = saidaX;
-			start_y = matrixSide - 3;
+			start_y = matrixSide - ((side % 2 == 0) ? 3 : 2);
 		}
 		matrix[start_y][start_x] = ' ';
 		
