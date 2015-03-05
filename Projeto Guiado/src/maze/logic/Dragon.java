592d6c1c48c06a218185c14aa8e9c5295241424d
gtugablue <gtugablue@gmail.com>
2015-03-05 01:33:01 +0000

    Added support for sleeping dragon.

--- Projeto Guiado/src/maze/logic/Dragon.java
+++ Projeto Guiado/src/maze/logic/Dragon.java
@@ -2,6 +2,7 @@ package maze.logic;
 
 public class Dragon extends Element{
 	private boolean alive = true;
+	private boolean sleeping = false;
 	
 	public Dragon(int x, int y) {
 		super(x, y);
@@ -14,4 +15,13 @@ public class Dragon extends Element{
 	public void setAlive(boolean alive) {
 		this.alive = alive;
 	}
+
+	public boolean isSleeping() {
+		return sleeping;
+	}
+
+	public void setSleeping(boolean sleeping) {
+		this.sleeping = sleeping;
+	}
+
 }
