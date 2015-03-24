8609c658430c1f074e259ade75a44a1d8f05bab8
aslmbc13 <asl_mbc_13@hotmail.com>
2015-03-24 16:29:11 +0000

    Improvs

---
+++ Projeto Guiado/src/maze/gui/GameFrame.java
@@ -0,0 +1,46 @@
+package maze.gui;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+	
+	String title = "Maze Escape";
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameFrame frame = new GameFrame();
+					frame.setVisible(true);
+
+					JPanel panel = new GamePanel();
+
+					frame.getContentPane().add(panel);
+					//frame.pack();
+					frame.setVisible(true);
+
+					panel.requestFocus(); // para receber eventos do teclado  
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GameFrame() {
+		super("ola");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 10, 500, 500);
+		/*contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);*/
+	}
+
+}
